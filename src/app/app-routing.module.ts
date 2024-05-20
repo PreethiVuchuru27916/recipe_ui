316dfdcbfc19b7a21d5a00b55f97cd17de50aa4e
Preethi Vuchuru <PreethiVuchuru@preethivuchuru.attlocal.net>
2024-05-19 19:02:24 -0500

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+
+const routes: Routes = [
+  { path: 'recipe', component: RecipeListComponent },
+  { path: 'shopping', component: ShoppingListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
